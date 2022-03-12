5e7ba8393d42b200a08cae1c835fa9b6ebb7b1d1
matthyk <matthias.keckl.develop@gmail.com>
2022-03-12 21:30:03 +0100

    fix state constructor

--- src/states/state-constructor.ts
+++ src/states/state-constructor.ts
@@ -1,9 +1,10 @@
 import { AbstractState } from './abstract-state'
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { IModel } from '../model'
+import { RequestModel } from './request-model'
 
 export type StateConstructor<
   Model extends IModel,
-  Request,
+  Request extends RequestModel,
   T extends AbstractState<Model, Request> = AbstractState<Model, Request>
 > = new (fastify: FastifyInstance, req: FastifyRequest<Request>, reply: FastifyReply) => T
